c9ecf5dd12902eaf982896459ea35462f6d15762
AHMEDmohamedyassin <ahmedmohamed982025@outlook.com>
2025-03-27 20:46:27 +0200

    v2

--- src/components/home/CarouselComp.jsx
+++ src/components/home/CarouselComp.jsx
@@ -1,47 +1,87 @@
-import React from 'react'
+import React, { useState } from 'react'
 import database from '../../database.json'
 
 const CarouselComp = () => {
+    const [active , setActive] = useState(0)
+
+    const navigate_carousel = (next = true) => {
+        let length = database.portfolio?.length
+
+        if(next && length && active < length -1)
+            setActive(active + 1)
+        else if (next && length && active == length - 1)
+            setActive(0)
+        else if (!next && length && active > 0)
+            setActive(active - 1)
+        else if (!next && length && active == 0)
+            setActive(length - 1 )
+    }
   return (
     <>
     
     
-    <div id="default-carousel" class="relative w-full" data-carousel="slide">
+    <div  className="relative w-full" >
 
-        <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+        <div className="relative h-[100vb] rounded-lg md:h-96">
 
             {
                 database.portfolio?.map((data , index) => (
-                    <div key={index} className="hidden duration-700 ease-in-out " data-carousel-item>
-                        <img src={data.image} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt={data.title}/>
+                    <div key={index}>
+
+                        {/* large screen carousel  */}
+                        <div className={`${index != active ? 'hidden' : ''} max-md:hidden absolute top-0 left-0 w-full h-full flex items-center justify-between px-20`} >
+                            {/* data */}
+                            <div className='flex-1 flex flex-col gap-4'>
+                                <h1 className='font-extrabold text-4xl dark:text-white '>{data.title}</h1>
+                                <p className='text-gray-500 dark:text-gray-400 font-semibold'>{data.description}</p>
+                            </div>
+                            {/* image  */}
+                            <div className='flex-1 relative w-full h-full'>
+                                <img src={data.image} className="absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 rounded-full h-[90%] w-[90%] shadow-xl shadow-gray-500/50 border-gray-500 border-[1px]" alt={data.title}/>
+                            </div>
+                        </div>
+
+                        {/* small screen carousel  */}
+                        <div className={`${index != active ? 'hidden' : ''} md:hidden absolute top-0 left-0 w-full h-full flex items-end p-4 py-20`} >
+                            {/* data */}
+                            <div className='flex flex-col gap-4 z-10 bg-white/50 p-10 rounded-xl shadow shadow-white'>
+                                <h1 className='font-extrabold text-4xl '>{data.title}</h1>
+                                <p className='text-gray-700 font-semibold'>{data.description}</p>
+                            </div>
+                            {/* image  */}
+                            <div className='absolute top-0 left-0 w-full h-full rounded-xl overflow-hidden'>
+                                <img src={data.image} className="h-full object-cover object-center" alt={data.title}/>
+                            </div>
+                        </div>
+
                     </div>
                 ) )
             }
 
         </div>
 
-        <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+        <div className="absolute z-30 flex -translate-x-1/2 md:bottom-0 bottom-2 left-1/2 space-x-3 rtl:space-x-reverse">
             {
                 database.portfolio?.map((data , index) => (
-                    <button key={index} type="button" className="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to={index}></button>
+                    <button onClick={() => setActive(index)} key={index} type="button" className={`${active == index ? 'bg-blue-600' : 'bg-gray-500/50'} hover:bg-blue-600 w-3 h-3 rounded-full `} ></button>
                 ) )
             }
         </div>
 
-        <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-gray-500/30 dark:bg-gray-300/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                <svg class="w-4 h-4 text-gray-500 dark:text-gray-300 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+        <button onClick={() => navigate_carousel(false)} className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" >
+            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-gray-500/30 dark:bg-gray-300/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                <svg className="w-4 h-4 text-gray-500 dark:text-gray-300 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
                 </svg>
-                <span class="sr-only">Previous</span>
+                <span className="sr-only">Previous</span>
             </span>
         </button>
-        <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-gray-500/30 dark:bg-gray-300/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                <svg class="w-4 h-4 text-gray-500 dark:text-gray-300 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+        <button onClick={navigate_carousel} type="button" className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" >
+            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-gray-500/30 dark:bg-gray-300/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                <svg className="w-4 h-4 text-gray-500 dark:text-gray-300 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
                 </svg>
-                <span class="sr-only">Next</span>
+                <span className="sr-only">Next</span>
             </span>
         </button>
     </div>
--- src/components/portfolio/CardComp.jsx
+++ src/components/portfolio/CardComp.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CardComp = ({title , description}) => {
   return (
     <div>
-        <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+        <div class=" p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 max-sm:flex flex-col items-center h-full">
             <a href="">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
             </a>
