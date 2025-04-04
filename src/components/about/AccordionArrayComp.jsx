0a446841765c54adb821da46f697445c6e7160dc
AHMEDmohamedyassin <ahmedmohamed982025@outlook.com>
2025-04-05 00:01:11 +0200

    v3

--- src/components/about/AccordionArrayComp.jsx
+++ src/components/about/AccordionArrayComp.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AccordionComp from './AccordionComp'
 import database from '../../database.json'
 
 const AccordionArrayComp = () => {
+  const [open , setOpen] = useState(0)
   return (
-    <div id="accordion-collapse" data-accordion="collapse">
+    <div className='border-gray-200 dark:border-gray-700 border-b-2'>
     {
         database.about?.map((e , index) => (
-            <AccordionComp id={index} {...e}/>
+            <AccordionComp id={index} {...e} open={open} setOpen={setOpen}/>
         ))
     }
     </div>
