508f00e0ced9e1cf19d7735b7724207c40f7bfeb
AHMEDmohamedyassin <ahmedmohamed982025@outlook.com>
2025-05-15 19:02:16 +0300

    v5

--- src/components/home/CarouselComp.jsx
+++ src/components/home/CarouselComp.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 import database from '../../database.json'
+import { useSwipeable } from 'react-swipeable'
 
-const CarouselComp = () => {
+const CarouselComp = () => {    
     const [active , setActive] = useState(0)
 
     const navigate_carousel = (next = true) => {
@@ -16,13 +17,21 @@ const CarouselComp = () => {
         else if (!next && length && active == 0)
             setActive(length - 1 )
     }
+
+    // swiping handle 
+    const handlers = useSwipeable({
+        onSwipedLeft : () => navigate_carousel() , 
+        onSwipedRight : () => navigate_carousel(false) , 
+        trackMouse:true
+    })
+
   return (
     <>
     
     
-    <div  className="relative w-full" >
+    <div {...handlers} className="relative w-full select-none" >
 
-        <div className="relative h-[100vb] rounded-lg md:h-96">
+        <div className="relative rounded-lg md:h-96">
 
             {
                 database.portfolio?.map((data , index) => (
@@ -42,15 +51,15 @@ const CarouselComp = () => {
                         </div>
 
                         {/* small screen carousel  */}
-                        <div className={`${index != active ? 'hidden' : ''} md:hidden absolute top-0 left-0 w-full h-full flex items-end p-4 py-20`} >
+                        <div className={`${index != active ? 'hidden' : ''} md:hidden  flex flex-col-reverse items-end justify-start gap-4 pb-10`} >
                             {/* data */}
                             <div className='flex flex-col gap-4 z-10 bg-white/50 p-10 rounded-xl shadow shadow-white'>
                                 <h1 className='font-extrabold text-4xl '>{data.title}</h1>
                                 <p className='text-gray-700 font-semibold'>{data.description}</p>
                             </div>
                             {/* image  */}
-                            <div className='absolute top-0 left-0 w-full h-full rounded-xl overflow-hidden'>
-                                <img src={data.image ?? database.default_img} className="h-full object-cover object-center" alt={data.title}/>
+                            <div className='w-2/4 aspect-video rounded-xl overflow-hidden -rotate-12'>
+                                <img src={data.image ?? database.default_img} className="object-contain" alt={data.title}/>
                             </div>
                         </div>
 
@@ -68,7 +77,7 @@ const CarouselComp = () => {
             }
         </div>
 
-        <button onClick={() => navigate_carousel(false)} className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" >
+        <button onClick={() => navigate_carousel(false)} className="max-md:hidden absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" >
             <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-gray-500/30 dark:bg-gray-300/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                 <svg className="w-4 h-4 text-gray-500 dark:text-gray-300 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
@@ -76,7 +85,7 @@ const CarouselComp = () => {
                 <span className="sr-only">Previous</span>
             </span>
         </button>
-        <button onClick={navigate_carousel} type="button" className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" >
+        <button onClick={navigate_carousel} type="button" className="max-md:hidden absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" >
             <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-gray-500/30 dark:bg-gray-300/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                 <svg className="w-4 h-4 text-gray-500 dark:text-gray-300 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
